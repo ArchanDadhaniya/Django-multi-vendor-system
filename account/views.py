13ef0c606cf34940c13a6d19f72437adaabeca82
ArchanDadhaniya <archandadhaniya@yahoo.com>
2025-08-10 11:28:14 +0530

    compelte the html inheritance

--- account/views.py
+++ account/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def registerUser(request):
+    return HttpResponse("User registration page")
+
+def registerRestaurant(request):
+    return HttpResponse("Restaurant registration page")
--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name="home"),
     path('about/', views.about, name="about"),
+    path('account/', include('account.urls')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
